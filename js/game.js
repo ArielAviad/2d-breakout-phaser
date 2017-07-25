4323116ada139509d0d7a5a175db85437886d3f4
ArielAviad <aviadrel@gmail.com>
2017-07-25 15:20:32 +0300

    add controller to the paddle

--- js/game.js
+++ js/game.js
@@ -4,6 +4,7 @@ var game = new Phaser.Game(480, 320, Phaser.AUTO, null,
 
 var ball;
 var paddle;
+var cursors;
 
 function loadImges() {
     game.load.image('ball', 'imgs/ball.png');
@@ -19,6 +20,8 @@ function preload() {
 
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    cursors = game.input.keyboard.createCursorKeys();
     //set start location and img.
     ball = game.add.sprite(100, 100, 'ball');
     paddle = game.add.sprite(game.world.width*0.5,game.world.height-5,
@@ -37,8 +40,26 @@ function create() {
 }
 
 function update() {
+    //Enable physics between the paddle and the ball
+    game.physics.arcade.collide(paddle,ball);
+
+
 }
 
 function render() {
     game.debug.spriteInfo(ball,32,32);
+
+    if (cursors.left.isDown)
+    {
+        paddle.body.velocity.x = -300;
+    }
+    else if (cursors.right.isDown)
+    {
+        paddle.body.velocity.x = 300;
+    }
+    else
+    {
+        paddle.body.velocity.setTo(0, 0);
+    }
+
 }
