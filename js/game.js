1f9b98f1a20cb77ad11cfb2b3bcb269f397f5a54
ArielAviad <aviadrel@gmail.com>
2017-07-25 16:19:58 +0300

    added bricks

--- js/game.js
+++ js/game.js
@@ -5,11 +5,14 @@ var game = new Phaser.Game(480, 320, Phaser.AUTO, null,
 var ball;
 var paddle;
 var cursors;
+var brickInfo;
 
 function loadImges() {
     game.load.image('ball', 'imgs/ball.png');
     game.load.image('paddle', 'imgs/paddle.png');
+    game.load.image('brick', 'imgs/brick.png');
 }
+
 function preload() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.pageAlignHorizontally = true;
@@ -28,6 +31,34 @@ function endGameRoles() {
     }, this);
 }
 
+function initBricks() {
+    brickInfo = {
+        width: 50,
+        height: 20,
+        count: {
+            row: 7,
+            col: 3
+        },
+        offset: {
+            top: 50,
+            left: 60
+        },
+        padding: 10
+    };
+    bricks = game.add.group();
+    for(var c=0;c<brickInfo.count.col;++c){
+        for(var r = 0;r<brickInfo.count.row;++r){
+            var brickX = (r*(brickInfo.width+brickInfo.padding))+brickInfo.offset.left;
+            var brickY = (c*(brickInfo.height+brickInfo.padding))+brickInfo.offset.top;
+            newBrick = game.add.sprite(brickX,brickY,'brick');
+            game.physics.enable(newBrick, Phaser.Physics.ARCADE);
+            newBrick.body.immovable = true;
+            newBrick.anchor.set(0.5);
+            bricks.add(newBrick);
+        }
+    }
+}
+
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
@@ -38,7 +69,7 @@ function create() {
                                 'paddle');
     //set on the buttom middle;
     ball.anchor.set(0.5);
-    paddle.anchor.set(0.5,1);
+    paddle.anchor.set(0.5);
     //  Enable Arcade Physics for the ball
     game.physics.enable([paddle, ball],Phaser.Physics.ARCADE);
 
@@ -50,6 +81,8 @@ function create() {
     ball.body.collideWorldBounds = true;
     //1 is 100% energy
     ball.body.bounce.setTo(1,1);
+
+    initBricks();
     endGameRoles.call(this);
 }
 
