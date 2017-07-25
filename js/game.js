f129b2542ef5c1b9d7fc50a203609f25eab4e1ba
ArielAviad <aviadrel@gmail.com>
2017-07-25 15:13:55 +0300

    add paddle

--- js/game.js
+++ js/game.js
@@ -3,21 +3,31 @@ var game = new Phaser.Game(480, 320, Phaser.AUTO, null,
     {preload: preload, create: create, update: update,render: render});
 
 var ball;
+var paddle;
 
+function loadImges() {
+    game.load.image('ball', 'imgs/ball.png');
+    game.load.image('paddle', 'imgs/paddle.png');
+}
 function preload() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
     game.stage.backgroundColor = '#eee';
-    game.load.image('ball','imgs/ball.png');
+    loadImges();
 }
 
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
     //set start location and img.
     ball = game.add.sprite(100, 100, 'ball');
+    paddle = game.add.sprite(game.world.width*0.5,game.world.height-5,
+                                'paddle');
     //  Enable Arcade Physics for the ball
-    game.physics.enable(ball,Phaser.Physics.ARCADE);
+    game.physics.enable([paddle, ball],Phaser.Physics.ARCADE);
+
+    paddle.body.immovable = true;
+
     //set gravity for x and y
     ball.body.velocity.set(100,100);
     //make the ball bounce-able
